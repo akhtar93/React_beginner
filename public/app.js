af6e1c73ca5304b5f620a4807767201499b7328d
Rashid Akhtar <akhtar.rashid93@gmail.com>
2023-10-30 23:37:09 +0530

    init commit

---
+++ public/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var template = React.createElement(
+  'p',
+  null,
+  'Hello! Welcome to ReactJs'
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
---
+++ src/index.js
@@ -0,0 +1,4 @@
+const template = <p>Hello! Welcome to ReactJs</p>
+const appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
